c4e227022f6611b6445cd8bb1d654fac6fbe59c1
ssogle <sobang91@gmail.com>
2021-06-02 01:24:38 +0900

    store data 추가

---
+++ store/skill.js
@@ -0,0 +1,85 @@
+export const state = () => ({
+  list: [
+    {
+      name: 'html',
+      level: 3,
+      explain:
+        '기획된 내용을 참고하여 적절한 태그와 함께 웹표준에 맞춰 웹문서를 작성 할 수 있습니다.',
+    },
+    {
+      name: 'css',
+      level: 3,
+      explain:
+        '기획된 내용을 참고하여 적절한 속성과 함께 크로스브라우징을 고려하여 애니메이션,반응형웹을 구현 할 수 있습니다.',
+    },
+    {
+      name: 'javascript',
+      level: 2,
+      explain: '다양한 웹어플리케이션을 제작 할 수 있습니다.',
+    },
+    {
+      name: 'jquery',
+      level: 2,
+      explain:
+        '상황에 맞는 프러그인.함수를 이용해 프론트엔드 개발을 편리하게,신속하게 할 수 있습니다.',
+    },
+    {
+      name: 'bootstrap',
+      level: 2,
+      explain:
+        '레퍼런스를 잘 이해하여 상황에 맞는 컴포넌트를 구성할 수 있습니다.',
+    },
+    {
+      name: 'nodejs',
+      level: 2,
+      explain:
+        '기본 모듈이나 npm을 통한 모듈을 설치, 활용하여 node.js 기반 어플리케이션을 제작할 수 있습니다.',
+    },
+    {
+      name: 'webpack',
+      level: 2,
+      explain:
+        '작업 환경을 세팅 할 수 있으며, 플러그인을 활용하여 웹사이트 제작에 활용할 수 있습니다.',
+    },
+    {
+      name: 'gulp',
+      level: 1,
+      explain:
+        '이미지 스프라이트 등의 플러그인을 활용하여 웹개발시 활용 가능합니다.',
+    },
+    {
+      name: 'react',
+      level: 2,
+      explain:
+        'webpack 또는 cli 를 사용하여 웹어플리케이션을 제작할 수 있습니다.',
+    },
+    {
+      name: 'php',
+      level: 2,
+      explain:
+        '기본적인 데이터베이스와 연동 작업이 가능하고, php를 기반으로한 프레임 워크 사용이 가능합니다.',
+    },
+    {
+      name: 'codeigniter',
+      level: 1,
+      explain:
+        'mvc모델을 파악하고있으며 해당 프레임워크로 웹사이트,관리자페이지를 제작할수있습니다.',
+    },
+    {
+      name: 'mysql',
+      level: 2,
+      explain:
+        'sql 쿼리 작성이 가능하며, php,node.js와 연동하여 데이터 활용을 할수있습니다.',
+    },
+    {
+      name: 'photoshop',
+      level: 1,
+      explain: '간단한 이미지 수정,제작이 가능합니다.',
+    },
+    {
+      name: 'Illustrator‎',
+      level: 1,
+      explain: '간단한 이미지 수정,제작이 가능합니다.',
+    },
+  ],
+})
