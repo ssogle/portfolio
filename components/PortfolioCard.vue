f56f5ed3cbb006a94801ec03e4986b552da9b7b3
ssogle <sobang91@gmail.com>
2021-06-02 03:54:47 +0900

    portfolio page 작업 - images 추가 - portfolio card componetn 추가 - portfolio store mapping & edit

---
+++ components/PortfolioCard.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="w-full p-3 break-words">
+    <div class="rounded overflow-hidden shadow-lg">
+      <img v-if="img" class="w-full" :src="require(`~/assets${img}`)" />
+      <div v-else class="text-center py-12 bg-blue-400 text-white">
+        <!-- <FontAwesomeIcon icon={faGrinAlt} size='3x' /> -->
+      </div>
+      <div class="px-6 py-4">
+        <span class="text-gray-500">{{ company }}</span>
+        <div class="font-bold text-xl mb-2">{{ name }}</div>
+        <p class="text-gray-700 text-base">
+          {{ content }}
+          <br />
+          <span class="text-blue-400">{{ desc }}</span>
+        </p>
+        <a
+          v-if="url"
+          class="text-gray-500"
+          href="{props.data.url}"
+          target="_blank"
+          rel="noopener noreferrer"
+          >{{ url }}</a
+        >
+      </div>
+      <div class="px-6 py-4">
+        <span
+          v-for="(item, index) in tag"
+          :key="index"
+          class="
+            inline-block
+            bg-gray-200
+            rounded-full
+            px-3
+            py-1
+            text-sm
+            font-semibold
+            text-gray-700
+            mr-2
+            mb-2
+          "
+          >#{{ item }}</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    company: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      default: '',
+    },
+    tag: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,9 +1,39 @@
 <template>
-  <div>portfolio page</div>
+  <section>
+    <div class="container m-auto">
+      <div class="text-gray-700">
+        <div class="text-center pt-5">
+          <!-- <FontAwesomeIcon icon={faBriefcase} size='3x' /> -->
+        </div>
+        <h2 class="text-center text-xl pt-2 pb-4 uppercase">portfolio</h2>
+      </div>
+      <!-- <PortfolioProvider>
+        <PortfolioList />
+      </PortfolioProvider> -->
+      <portfolio-card
+        v-for="(item, index) in list"
+        :key="index"
+        :company="item.company"
+        :img="item.img"
+        :name="item.name"
+        :content="item.content"
+        :desc="item.desc"
+        :url="item.url"
+        :tag="item.tag"
+      ></portfolio-card>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {}
+import { mapState } from 'vuex'
+import PortfolioCard from '../components/PortfolioCard.vue'
+export default {
+  components: { PortfolioCard },
+  computed: {
+    ...mapState('portfolio', ['list']),
+  },
+}
 </script>
 
 <style></style>
