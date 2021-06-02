a74ae8eca377af34ce9893984c9ff7b18931e849
ssogle <sobang91@gmail.com>
2021-06-03 05:32:30 +0900

    store add - 공통 메뉴 데이터 추가

---
+++ store/common.js
@@ -0,0 +1,29 @@
+export const state = () => ({
+  menuList: [
+    {
+      name: 'home',
+      path: '/',
+      url: null,
+    },
+    {
+      name: 'career',
+      path: '/career',
+      url: null,
+    },
+    {
+      name: 'skill',
+      path: '/skill',
+      url: null,
+    },
+    {
+      name: 'portfolio',
+      path: '/portfolio',
+      url: null,
+    },
+    {
+      name: 'github',
+      path: null,
+      url: 'https://github.com/ssogle/portfolio',
+    },
+  ],
+})
